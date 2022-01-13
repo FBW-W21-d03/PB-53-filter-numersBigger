fed37eae4209eec9831b860e4ab1e20b73f8b579
Vens <v.kolev@alfatrex.com>
2022-01-13 09:18:49 +0100

    vens

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 // ## Aufgabenstellung - Teil 1
 // Schreibe ein Array mit folgenden Zahlen: 80, 120, 90, 130, 145, 99, 85, 125, 132.
 const zifer = [80, 120, 90, 130, 145, 99, 85, 125, 132];
+zifer.sort();
 // ## Aufgabenstellung - Teil 2
 // Wende auf das Array nun die filter-Methode an, um alle Zahlen über 100 in ein neues Array zu filtern.
 const filter = zifer.filter(checkZifer);
