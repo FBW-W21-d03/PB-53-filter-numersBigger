57f4b700e44f9696af4ee7777d1604d746f56c64
sprotte <metatron1972@live.de>
2022-01-13 09:54:45 +0100

    sprotte

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 "use strict";
 
-const array = [80, 120, 90, 130, 145, 99, 85, 125, 132];
-const arrayFiltered = array.filter((zahl) => zahl > 100);
-console.log(arrayFiltered);
-const arrayFiltered2 = array.filter((zahl) => zahl > 100 && zahl <= 140);
-console.log(arrayFiltered2);
+const zahln = [80, 120, 90, 130, 145, 99, 85, 125, 132];
+const zahlueber = zahln.filter((zahl) => zahl > 100);
+console.log(zahlenueber);
+const zahlenueber = zahlueber.filter((zahl) => zahl > 100 && zahl <= 140);
+console.log(zahlenunter);
