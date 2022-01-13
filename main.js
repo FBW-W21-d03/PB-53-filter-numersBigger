5b2ead5bccadd26e7a1dfe4b847100289109da4c
kawanaza <kawanbabani@gmail.com>
2022-01-13 09:58:40 +0100

    done

--- main.js
+++ main.js
@@ -1,8 +1,19 @@
 const number = [80, 120, 90, 130, 145, 99, 85, 125, 132];
 const top100 = number.filter((item) => item <= 100);
-const between = number.filter((elem) => (elem) > 100 && 140);
+const between = number.filter((elem) => (elem) > 100 && elem < 140);
 
 console.log(top100);
 console.log(between);
 
+function myfunction(topNumber) {
+  if (number => 100) {
+    return topNumber >= 100;
+  } else if (number< 140) {
+    return topNumber <= 140;
+  }
+}
+
+console.log(number);
+console.log(top100);
+console.log(between);
 
