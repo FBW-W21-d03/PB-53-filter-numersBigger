13e0a49d721f6479ba76b24e729665c13c68b443
OrhanKadirov <orhan.kadirov.gs@gmail.com>
2022-01-13 09:55:34 +0100

    Orhan's lösung

--- javaScript-filter-numbersBigger.js
+++ javaScript-filter-numbersBigger.js
@@ -4,10 +4,5 @@ console.log("Aufgabe 1:\n", zahlen);
 const gröser100 = zahlen.filter((zahl) => zahl >= 100);
 console.log("Aufgabe 2:\n", gröser100);
 
-function isBigEnough(zahlen) {
-    return zahlen >= 100 && zahlen <= 140; 
-    
-}
-
-const zwischen100Und140 = zahlen.filter(isBigEnough);
+const zwischen100Und140 = zahlen.filter( (zahl) => zahl >= 100 && zahl <= 140);
 console.log("Aufgabe 3:\n", zwischen100Und140);
