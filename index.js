90c2ffef5aedd96fc841beb9970bd7b14e9fdf8f
aisha2000-hub <aisha.noori2000@gmail.com>
2022-01-13 09:26:10 +0100

    Dis lösung von Aisha

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const numbers = [80, 120, 90, 130, 145, 99, 85, 125, 132];
 
+numbers.sort()
 const newNumbers = numbers.filter((number) => number > 100);
 const newNumbers2 = numbers.filter((number) => number > 100 && number < 140);
 
-console.log("newNUmberArray:",`${newNumbers}`);
+console.log("newNUmberArray1:",`${newNumbers}`);
 console.log("newNUmberArray2:", `${newNumbers2}`);
